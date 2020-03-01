97221847bfd95f0b9272e4b0399902ebafcbfcc6
aleksandrdremov <dremov.me@gmail.com>
2020-03-01 12:30:48 +0300

    Format

--- Shadow.cpp
+++ Shadow.cpp
@@ -9,8 +9,7 @@
 #include <Shadow.h>
 #include <cppQueue.h>
 
-Shadow::Shadow(int port0, int port1, int port2, int port3, int steps, int fullSteps, int triggerPort, String id)
-{
+Shadow::Shadow(int port0, int port1, int port2, int port3, int steps, int fullSteps, int triggerPort, String id) {
     if (triggerPort != -1)
         pinMode(triggerPort, INPUT);
     this->reversed = 1;
@@ -41,29 +40,24 @@ Shadow::Shadow(int port0, int port1, int port2, int port3, int steps, int fullSt
 
 Shadow::Shadow() {}
 
-void Shadow::fullUp()
-{
+void Shadow::fullUp() {
     bool trigger = this->triggerPort != -1;
     this->addToQue(0, this->getSpeed(), this->getAcceleration(), trigger);
 }
 
-void Shadow::fullDown()
-{
+void Shadow::fullDown() {
     this->addToQue(this->fullSteps, this->getSpeed(), this->getAcceleration(), false);
 }
 
-int Shadow::getPosition()
-{
+int Shadow::getPosition() {
     return this->position;
 }
 
-void Shadow::setPosition(int pos)
-{
+void Shadow::setPosition(int pos) {
     this->position = pos;
 }
 
-void Shadow::calibrate(float speed /*= 1*/, float finalSpeed /*= 0.5*/)
-{
+void Shadow::calibrate(float speed /*= 1*/, float finalSpeed /*= 0.5*/) {
     // this->setSpeed(speed);
     // int currentPos = this->getPosition();
 
@@ -90,87 +84,69 @@ void Shadow::calibrate(float speed /*= 1*/, float finalSpeed /*= 0.5*/)
     // this->setPosition(0);
 }
 
-void Shadow::setSpeed(float speed)
-{
+void Shadow::setSpeed(float speed) {
     this->speed = speed;
     this->maxSpeed = speed;
     this->motor.setSpeed(speed);
     this->motor.setMaxSpeed(this->maxSpeed);
 }
 
-void Shadow::reverse(int reversed)
-{
+void Shadow::reverse(int reversed) {
     this->reversed = reversed;
 }
 
-void Shadow::setTarget(path target)
-{
+void Shadow::setTarget(path target) {
     this->currentJob = target;
 }
 
-bool Shadow::isTriggered()
-{
+bool Shadow::isTriggered() {
     if (this->triggerPort == -1)
         return false;
     return digitalRead(this->triggerPort) == HIGH;
 }
 
-void Shadow::loop()
-{
+void Shadow::loop() {
     this->motor.run();
     this->retreiveWork();
     this->updatePosition();
-    if (!this->noWork)
-    {
-        if (this->currentJob.tillTriger && this->isTriggered())
-        {
+    if (!this->noWork) {
+        if (this->currentJob.tillTriger && this->isTriggered()) {
             this->currentJob.tillTriger = false;
             this->noWork = true;
             this->motor.stop();
             return;
         }
-        if (this->currentJob.tillTriger)
-        {
+        if (this->currentJob.tillTriger) {
             this->motor.move(this->singleLoop * this->reversed * this->currentJob.direction);
         }
     }
-    if (!this->motor.run())
-    {
+    if (!this->motor.run()) {
         this->noWork = true;
         this->motor.disableOutputs();
         this->currentState = 2;
-    }
-    else
-    {
+    } else {
         this->printDebug();
         if (this->motor.distanceToGo() * this->reversed > 0)
             this->currentState = 1;
-        else
-        {
+        else {
             this->currentState = 0;
         }
     }
 }
 
-void Shadow::retreiveWork()
-{
-    if (this->noWork)
-    {
+void Shadow::retreiveWork() {
+    if (this->noWork) {
         if (this->queue.isEmpty())
             return;
-        else
-        {
+        else {
             this->noWork = false;
             path task;
             this->queue.pop(&task);
             this->setSpeed(task.speed);
             this->setAcceleration(task.acceleration);
-            if (task.tillTriger)
-            {
+            if (task.tillTriger) {
                 this->motor.move(this->singleLoop * this->reversed * task.direction);
-            }
-            else
-            {
+            } else {
                 this->motor.moveTo(task.position * this->reversed);
             }
             this->motor.enableOutputs();
@@ -179,10 +155,8 @@ void Shadow::retreiveWork()
     }
 }
 
-void Shadow::printDebug()
-{
-    if (this->debug)
-    {
+void Shadow::printDebug() {
+    if (this->debug) {
         path *target = this->getTarget();
         Serial.print("*******\n");
         Serial.print(this->getId() + ": target->");
@@ -196,94 +170,79 @@ void Shadow::printDebug()
     }
 }
 
-path *Shadow::getTarget()
-{
+path *Shadow::getTarget() {
     return &this->currentJob;
 }
 
-void Shadow::setAcceleration(float acceleration)
-{
+void Shadow::setAcceleration(float acceleration) {
     this->acceleration = acceleration;
     this->motor.setAcceleration(acceleration);
 }
 
-float Shadow::getSpeed()
-{
+float Shadow::getSpeed() {
     return this->speed;
 }
 
-void Shadow::addTargetPercent(float percent)
-{
+void Shadow::addTargetPercent(float percent) {
     int pos = int(percent * float(this->fullSteps) / 100.00);
     this->addTargetPosition(pos);
 }
 
-void Shadow::addTargetPosition(int pos)
-{
+void Shadow::addTargetPosition(int pos) {
     this->addToQue(pos, this->getSpeed(), this->getAcceleration(), false);
     this->noWork = false;
 }
 
-void Shadow::setTargetPercent(float percent)
-{
+void Shadow::setTargetPercent(float percent) {
     int pos = int(percent * float(this->fullSteps) / 100.00);
     this->setTargetPosition(pos);
 }
 
-void Shadow::setTargetPosition(int pos)
-{
+void Shadow::setTargetPosition(int pos) {
     this->stop();
     this->addToQue(pos, this->getSpeed(), this->getAcceleration(), false);
     this->noWork = false;
 }
 
-void Shadow::stop()
-{
+void Shadow::stop() {
     this->clearQueue();
     this->motor.stop();
     this->noWork = true;
 }
 
-String Shadow::getId()
-{
+String Shadow::getId() {
     return this->id;
 }
 
-int Shadow::getPositionPercent()
-{
+int Shadow::getPositionPercent() {
     if (this->fullSteps == 0)
         return 0;
     float all = float(this->fullSteps);
     return int((float(this->getPosition()) / all) * 100.0);
 }
 
-int Shadow::queueLength()
-{
+int Shadow::queueLength() {
     return this->queue.getCount();
 }
 
-bool Shadow::jobExists()
-{
+bool Shadow::jobExists() {
     return !this->noWork;
 }
-void Shadow::clearQueue()
-{
+
+void Shadow::clearQueue() {
     this->queue.clean();
     this->queue.flush();
 }
 
-int Shadow::getState()
-{
+int Shadow::getState() {
     return this->currentState;
 }
 
-void Shadow::updatePosition()
-{
+void Shadow::updatePosition() {
     this->setPosition(this->motor.currentPosition() * this->reversed);
 }
 
-void Shadow::addToQue(int position, float speed, float acceleration, bool tillTriger)
-{
+void Shadow::addToQue(int position, float speed, float acceleration, bool tillTriger) {
     path point;
     point.speed = speed;
     point.position = position;
@@ -291,21 +250,18 @@ void Shadow::addToQue(int position, float speed, float acceleration, bool tillTr
     point.tillTriger = tillTriger;
     if (this->getPosition() < position)
         point.direction = 1;
-    else
-    {
+    else {
         point.direction = -1;
     }
     this->noWork = false;
     this->queue.push(&point);
 }
 
-float Shadow::getAcceleration()
-{
+float Shadow::getAcceleration() {
     return this->acceleration;
 }
 
-void Shadow::setDebug(bool debug)
-{
+void Shadow::setDebug(bool debug) {
     this->debug = debug;
 }
 
